3975b894001a406ce181dd310de1d7fe02a1d85b
William Bruno <willbc.silva@gmail.com>
2024-09-08 19:48:33 -0300

    new_projects

---
+++ .vscode/.python/PySimpleGUI/busca_OLX/busca_xboxs.py
@@ -0,0 +1,59 @@
+from PySimpleGUI import PySimpleGUI as sg
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from time import sleep
+from datetime import datetime
+import pyperclip
+
+sg.theme('Reddit')
+
+def resultados_busca(dados): 
+    layout = [
+        [sg.Table(values=dados, headings=("Título", "Data", "Preço", "Link"), justification= 'Left', auto_size_columns = True, num_rows = 30, expand_x=True, enable_events=True, expand_y=True, key='-TABELA-', select_mode = sg.SELECT_MODE_BROWSE)],
+        [sg.Button('Copiar Link'), sg.Button('Fechar')]
+    ]
+    janela = sg.Window("OLX - Resultados da busca | Xbox Series S", layout, size=(800,600)) 
+
+    linha_selecionada = None
+
+    while True:
+        eventos, valores = janela.read()
+        if eventos == sg.WINDOW_CLOSED or eventos == 'Fechar':
+            break
+        #Garantir que ele pegue linhas preenchidas
+        if eventos == '-TABELA-':            
+            if valores['-TABELA-']:
+                linha_selecionada = valores['-TABELA-'][0]
+            else: 
+                None
+        if eventos == 'Copiar Link' and linha_selecionada is not None:          
+            link_copiado = dados[linha_selecionada][3]  # Índice 3 que é a coluna do link
+            pyperclip.copy(link_copiado)
+            sg.popup('Link copiado para a área de transferência!') 
+        
+    janela.close()
+
+resultado = []
+
+driver = webdriver.Chrome()
+driver.get('https://www.olx.com.br/games/consoles-de-video-game/estado-sp/baixada-santista-e-litoral-sul/santos?q=xbox%20series%20S')
+sleep(6)
+
+button_aceita = driver.find_element(By.XPATH, "//button[@id='adopt-accept-all-button']")
+button_aceita.click()
+
+valores = driver.find_elements(By.XPATH, "//h3[@data-ds-component ='DS-Text']")
+titulos = driver.find_elements(By.XPATH, "//h2[@data-ds-component ='DS-Text']")
+links = driver.find_elements(By.XPATH, "//a[@class ='olx-ad-card__title-link']")
+
+
+for valor, titulo, link in zip(valores, titulos, links):
+    valor_pronto = valor.text
+    titulo_pronto = titulo.text
+    link_pronto = link.get_attribute('href')
+    data_atual = datetime.now().strftime('%d/%m/%Y')
+    resultado.append([titulo_pronto, data_atual, valor_pronto, link_pronto])
+
+driver.quit()
+
+resultados_busca(resultado)
--- .vscode/.python/projeto_verifica_CPF/automacao_CPF.py
+++ .vscode/.python/projeto_verifica_CPF/automacao_CPF.py
@@ -2,10 +2,26 @@ import openpyxl  # biblioteca para abrir excel
 from selenium import webdriver  # biblioteca para acesso a internet
 from selenium.webdriver.common.by import By  # Permite encontrar elementos dentro da página
 from time import sleep
+from PySimpleGUI import PySimpleGUI as sg
 #from selenium.webdriver.support.ui import WebDriverWait
 #from selenium.webdriver.support import expected_conditions as EC
 
-# Abrir planilha e a aba
+sg.theme('Reddit')
+def resultados_tela(dados):
+    layout = [
+        [sg.Table(values=dados, headings=["Nome", "CPF", "Status"], auto_size_columns = True, num_rows=10, justification="left")],
+        [sg.Button('Fechar')]
+    ]
+    janela = sg.Window('Validador de CPF', layout)
+
+    while True:
+        eventos, valores = janela.read()
+        if eventos == sg.WINDOW_CLOSED or eventos == 'Fechar':
+            break
+    
+    janela.close()
+
+# Abrir planilha e indicar a aba
 planilha_CPF = openpyxl.load_workbook('C:/Users/Myrath/Desktop/Projetos_VSCode/.vscode/.python/projeto_verifica_CPF/cpf_clientes.xlsx')
 pagina_CPF = planilha_CPF['Sheet1']
 
@@ -16,6 +32,8 @@ sleep(4)
 
 validacao_CPF = openpyxl.load_workbook('C:/Users/Myrath/Desktop/Projetos_VSCode/.vscode/.python/projeto_verifica_CPF/validacao_CPF.xlsx')
 pagina_validacao = validacao_CPF['Sheet1']
+
+resultados = []
 
 # Ler as linhas a partir da linha 02 e processar o Nome e CPF
 for linha in pagina_CPF.iter_rows(min_row=2, values_only=True):
@@ -52,8 +70,12 @@ for linha in pagina_CPF.iter_rows(min_row=2, values_only=True):
     else:
         status_validacao = 'CPF Inválido!'
     
+    resultados.append([nome, cpf, status_validacao])
+
     pagina_validacao.append([nome, cpf, status_validacao])
     
 validacao_CPF.save('C:/Users/Myrath/Desktop/Projetos_VSCode/.vscode/.python/projeto_verifica_CPF/validacao_CPF.xlsx')
 # Fecha o navegador após acabar a rotina
 driver.quit()
+
+resultados_tela(resultados)
